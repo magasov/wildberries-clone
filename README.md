6a189e152a781d93388c825ff778836b6558dac8
magasov <77aushev@mail.ru>
2024-12-23 15:40:46 +0300

    styles lk

--- README.md
+++ README.md
@@ -4,14 +4,12 @@
       <img src="./src/assets/readme/banner-readme.png" alt="Project Banner">
     </a>
   <br />
-  <br />
  
-  <div>
-    <img src="https://camo.githubusercontent.com/9242244a22ff5237b5410291986a5d911e829e494f47913c7d9bd0404627d789/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f612f61372f52656163742d69636f6e2e737667" alt="react.js" width="30x" />
-    <img src="https://camo.githubusercontent.com/e84d110dc8fc6125b9138856352724ba0f8f6b86ec6ac91961669d407fd71e24/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f353936382f353936383335382e706e67" alt="scss" width="28x" />
-  </div>
-<br>
-  <h3 align="center">Clone WB</h3>
+  <p>
+    <code><img src="https://camo.githubusercontent.com/9242244a22ff5237b5410291986a5d911e829e494f47913c7d9bd0404627d789/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f612f61372f52656163742d69636f6e2e737667" alt="react.js" height="30" /></code>
+    <code><img src="https://camo.githubusercontent.com/e84d110dc8fc6125b9138856352724ba0f8f6b86ec6ac91961669d407fd71e24/68747470733a2f2f63646e2d69636f6e732d706e672e666c617469636f6e2e636f6d2f3531322f353936382f353936383335382e706e67" alt="scss" height="30" /></code>
+  </p>
+  <h1 align="center">Clone WB</h1>
 
    <div align="center">
      Это клонированная версия популярного интернет-магазина Wildberries, созданная с целью изучения и применения различных концепций и технологий, связанных с разработкой пользовательских интерфейсов на React.
@@ -29,6 +27,10 @@
 7. 🚀 Swiper
 8. 🎨 Sass
 9. 🧩 Concurrently
+
+## 🌐 <a name="table">Lk</a>
+
+<img src="./src/assets/readme/lk.png" alt="lk">
 
 ##
 
--- src/pages/myProfile/MyProfile.jsx
+++ src/pages/myProfile/MyProfile.jsx
@@ -1,115 +1,164 @@
-import React from 'react'
+import React from "react";
 
-import "./myProfile.scss"
+import "./myProfile.scss";
 import avatar from "../../assets/logo/avatar.jpg";
-import CloseIcon from '@mui/icons-material/Close';
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import SettingsIcon from '@mui/icons-material/Settings';
-import MonitorIcon from '@mui/icons-material/Monitor';
-import CreditScoreIcon from '@mui/icons-material/CreditScore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import GradeIcon from '@mui/icons-material/Grade';
+import CloseIcon from "@mui/icons-material/Close";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import SettingsIcon from "@mui/icons-material/Settings";
+import MonitorIcon from "@mui/icons-material/Monitor";
+import CreditScoreIcon from "@mui/icons-material/CreditScore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
+import GradeIcon from "@mui/icons-material/Grade";
+import Rozygraysh from "../../assets/banner/rozygrush.png";
+import MessageIcon from "@mui/icons-material/Message";
+import HomeRepairServiceIcon from "@mui/icons-material/HomeRepairService";
+import LiveHelpIcon from "@mui/icons-material/LiveHelp";
 
 const MyProfile = ({ user, setUser }) => {
-    const [popup, setPopup] = React.useState(false)
+  const [popup, setPopup] = React.useState(false);
   return (
     <div className="background__profile">
-        <div className="container">
-            <div className='myProfile'>
-                <div className="myProfile__left">
-                    <div onClick={() => setPopup(true)} className="profile">
-                        <img src={user.avatar || avatar} alt="avatar" />
-                        {user.name}
-                    </div>
-                    <div className="profile_div">
-                        <div>
-                            WB скидка
-                            <b>до 30%</b>
-                        </div> 
-                        <div>
-                            Оплата при получении
-                            <b>до 35 000 ₽</b>
-                        </div>
-                    </div>
-                    <div className='profile_finance'>
-                        <p>Финансы</p>
-                        <div><CreditCardIcon /> Способы оплаты</div>
-                        <div><AssignmentIcon /> Реквизиты</div>
-                    </div>
-                    <div className='profile_finance'>
-                        <p>Управление</p>
-                        <div><SettingsIcon /> Настройки</div>
-                        <div><MonitorIcon /> Ваши устройства</div>
-                    </div>
-                </div>
-                <div className="myProfile__right">
-                    <div className="myProfile__right-one">
-                        <div className='myProfile__right-one-div'>
-                            <div>
-                                <p><CreditScoreIcon /> 0 ₽</p>
-                                <span>WB Кошелёк</span>
-                            </div>
-                            <button>Пополнить</button>
-                        </div>
-                        <div className='myProfile__right-one-div'>
-                            <div>
-                                <p>Рассрочка</p>
-                                <span>До 50 000 ₽</span>
-                            </div>
-                            <span>
-                                <ChevronRightIcon />
-                            </span>
-                        </div>
-                    </div>
-                    <div className="myProfile__right-two">
-                        <div className='myProfile__right-two-img'>
-                            <h3>Выиграйте <br /> миллион <span>на покупки WB</span></h3>
-                        </div>
-                        <div className='myProfile__right-two-div'></div>
-                    </div>
-                    <div className="myProfile__right-one">
-                        <div className='myProfile__right-one-div'>
-                            <div>
-                                <p>Избранное</p>
-                                <span>2 товара</span>
-                            </div>
-                            <span className='lk_icons'><FavoriteIcon /></span>
-                        </div>
-                        <div className='myProfile__right-one-div'>
-                            <div>
-                                <p>Покупки</p>
-                                <span>Смотреть</span>
-                            </div>
-                            <span className='lk_icons'>
-                                <ShoppingBagIcon />
-                            </span>
-                        </div>
-                            <div className='myProfile__right-one-div'>
-                                <div>
-                                    <p>Ждут оценки</p>
-                                    <span>4 товара</span>
-                                </div>
-                                <span className='lk_icons'>
-                                    <GradeIcon />
-                                </span>
-                            </div>
-                    </div>
-                </div>
+      <div className="container">
+        <div className="myProfile">
+          <div className="myProfile__left">
+            <div onClick={() => setPopup(true)} className="profile">
+              <img src={user.avatar || avatar} alt="avatar" />
+              {user.name}
             </div>
+            <div className="profile_div">
+              <div>
+                WB скидка
+                <b>до 30%</b>
+              </div>
+              <div>
+                Оплата при получении
+                <b>до 35 000 ₽</b>
+              </div>
+            </div>
+            <div className="profile_finance">
+              <p>Финансы</p>
+              <div>
+                <CreditCardIcon /> Способы оплаты
+              </div>
+              <div>
+                <AssignmentIcon /> Реквизиты
+              </div>
+            </div>
+            <div className="profile_finance">
+              <p>Управление</p>
+              <div>
+                <SettingsIcon /> Настройки
+              </div>
+              <div>
+                <MonitorIcon /> Ваши устройства
+              </div>
+            </div>
+          </div>
+          <div className="myProfile__right">
+            <div className="myProfile__right-one">
+              <div className="myProfile__right-one-div">
+                <div>
+                  <p>
+                    <CreditScoreIcon /> {user.balance} ₽
+                  </p>
+                  <span>WB Кошелёк</span>
+                </div>
+                <button>Пополнить</button>
+              </div>
+              <div className="myProfile__right-one-div">
+                <div>
+                  <p>Рассрочка</p>
+                  <span>До 50 000 ₽</span>
+                </div>
+                <span>
+                  <ChevronRightIcon />
+                </span>
+              </div>
+            </div>
+            <div className="myProfile__right-two">
+              <div className="myProfile__right-two-img">
+                <h3>
+                  Выиграйте <br /> миллион <span>на покупки WB</span>
+                </h3>
+                <img src={Rozygraysh} alt="img" />
+              </div>
+              <div className="myProfile__right-two-div">
+                <div>
+                  <h4>Не собрали шансов</h4>
+                </div>
+                <div className="div__result">
+                  <p>Акция окончена</p>
+                  <span>Результаты в эфире</span>
+                </div>
+              </div>
+            </div>
+            <div className="myProfile__right-one">
+              <div className="myProfile__right-one-div">
+                <div>
+                  <p>Избранное</p>
+                  <span>2 товара</span>
+                </div>
+                <span className="lk_icons">
+                  <FavoriteIcon />
+                </span>
+              </div>
+              <div className="myProfile__right-one-div">
+                <div>
+                  <p>Покупки</p>
+                  <span>Смотреть</span>
+                </div>
+                <span className="lk_icons">
+                  <ShoppingBagIcon />
+                </span>
+              </div>
+              <div className="myProfile__right-one-div">
+                <div>
+                  <p>Ждут оценки</p>
+                  <span>4 товара</span>
+                </div>
+                <span className="lk_icons">
+                  <GradeIcon />
+                </span>
+              </div>
+            </div>
+            <div className="myProfile__right-three">
+              <h3 className="lk_title">Сервис и помощь</h3>
+              <div className="lk_justify">
+                <div>
+                  <MessageIcon />
+                  Написать в поддержку
+                </div>
+                <div>
+                  <HomeRepairServiceIcon />
+                  Вернуть товар
+                </div>
+                <div>
+                  <LiveHelpIcon />
+                  Вопросы и ответы
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        {popup && <form className="background__profile-popup">
-            <div>
-                <CloseIcon onClick={() => setPopup(false)} className='popup_close' />
-                <h2>Личные данные</h2>
-                <input type="text" placeholder='Имя' />
-                <button>Сохранить</button>
-            </div>
-        </form>}
+      </div>
+      {popup && (
+        <form className="background__profile-popup">
+          <div>
+            <CloseIcon
+              onClick={() => setPopup(false)}
+              className="popup_close"
+            />
+            <h2>Личные данные</h2>
+            <input type="text" placeholder="Имя" />
+            <button>Сохранить</button>
+          </div>
+        </form>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MyProfile
+export default MyProfile;
